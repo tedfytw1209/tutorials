829129aeac2a3d1af4017234be6f8b7a05aea802
teddy <teddytw1209@gmail.com>
2024-06-08 18:49:11 +0800

    LayerNorm change in pyramid

--- detection/network/vitdet.py
+++ detection/network/vitdet.py
@@ -162,6 +162,28 @@ def add_decomposed_rel_pos(attn: Tensor, q: Tensor, rel_pos_h: Tensor, rel_pos_w
 
     return attn
 
+class LayerNorm(nn.Module):
+    """
+    A LayerNorm variant, popularized by Transformers, that performs point-wise mean and
+    variance normalization over the channel dimension for inputs that have shape
+    (batch_size, channels, height, width).
+    https://github.com/facebookresearch/ConvNeXt/blob/d1fa8f6fef0a165b27399986cc2bdacc92777e40/models/convnext.py#L119  # noqa B950
+    """
+
+    def __init__(self, normalized_shape: int, eps: float=1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        u = x.mean(1, keepdim=True)
+        s = (x - u).pow(2).mean(1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.eps)
+        x = self.weight[:, None, None] * x + self.bias[:, None, None]
+        return x
+
 class SABlock(nn.Module): ###!!! Not checked
     """
     A self-attention block, based on: "Dosovitskiy et al.,
@@ -556,7 +578,7 @@ class SimpleFeaturePyramid(nn.Module): ###!!! Not checked
             if scale == 4.0:
                 layers = [
                     nn.ConvTranspose2d(dim, dim // 2, kernel_size=2, stride=2),
-                    nn.LayerNorm(dim // 2,eps=1e-6), #!!! detectron2 use 1e-6
+                    LayerNorm(dim // 2,eps=1e-6), #!!! detectron2 use 1e-6
                     nn.GELU(),
                     nn.ConvTranspose2d(dim // 2, dim // 4, kernel_size=2, stride=2),
                 ]
@@ -579,7 +601,7 @@ class SimpleFeaturePyramid(nn.Module): ###!!! Not checked
                         kernel_size=1,
                         bias=use_bias,
                     ),
-                    nn.LayerNorm(out_channels,eps=1e-6), #!!! detectron2 use 1e-6
+                    LayerNorm(out_channels,eps=1e-6), #!!! detectron2 use 1e-6
                     nn.Conv2d(
                         out_channels,
                         out_channels,
