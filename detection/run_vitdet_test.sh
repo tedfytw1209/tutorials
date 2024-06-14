d520137aa8c9d1bf18f03cc054fc72da5d1a3169
teddy <teddytw1209@gmail.com>
2024-06-14 11:13:03 +0800

    for check

---
+++ detection/run_vitdet_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --partition=gpu
+#SBATCH --gpus=a100:1
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+
+date;hostname;pwd
+
+module load singularity
+
+# For debug purpose, check if CUDA is currently available for torch. If available, will return `True`.
+singularity exec --nv /blue/bianjiang/tienyuchang/monaicore1.3.0 python -c "import torch; print(torch.cuda.is_available())"
+
+# Run a tutorial python script within the container. Modify the path to your container and your script.
+singularity exec --nv /blue/bianjiang/tienyuchang/monaicore1.3.0 python luna16_vit_testing.py -e ./config/environment_vit_luna16_fold0.json -c ./config/config_infer_vitdet2dimg_luna16_80g.json -d -m /blue/bianjiang/tienyuchang/basemodel/checkpoint_test.pth 
