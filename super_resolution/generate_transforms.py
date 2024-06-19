4b0c875329f17f4a81f0eeb9e9d33135839d87c6
teddy <teddytw1209@gmail.com>
2024-06-19 23:58:50 +0800

    ssim debug

--- super_resolution/generate_transforms.py
+++ super_resolution/generate_transforms.py
@@ -22,6 +22,46 @@ from torchvision.transforms.functional import rgb_to_grayscale
 
 from torcheval.metrics.image import PeakSignalNoiseRatio
 from torcheval.metrics.image.ssim import StructuralSimilarity
+from skimage.metrics import structural_similarity
+from typing import Iterable, Optional, TypeVar
+TStructuralSimilarity = TypeVar("TStructuralSimilarity")
+
+class StructuralSimilarity_gray(StructuralSimilarity):
+    @torch.inference_mode()
+    # pyre-ignore[14]: `update` overrides method defined in `Metric` inconsistently.
+    def update(
+        self: TStructuralSimilarity,
+        images_1: torch.Tensor,
+        images_2: torch.Tensor,
+    ) -> TStructuralSimilarity:
+        """
+        Update the metric state with new input.
+        Ensure that the two sets of images have the same value range (ex. [-1, 1], [0, 1]).
+
+        Args:
+            images_1 (Tensor): A batch of the first set of images of shape [N, C, H, W].
+            images_2 (Tensor): A batch of the second set of images of shape [N, C, H, W].
+
+        """
+        if images_1.shape != images_2.shape:
+            raise RuntimeError("The two sets of images must have the same shape.")
+        # convert to fp32, mostly for bf16 types
+        images_1 = images_1.to(dtype=torch.float32)
+        images_2 = images_2.to(dtype=torch.float32)
+
+        batch_size = images_1.shape[0]
+
+        for idx in range(batch_size):
+            mssim = structural_similarity(
+                images_1[idx].squeeze().detach().cpu().numpy(),
+                images_2[idx].squeeze().detach().cpu().numpy(),
+                multichannel=False,
+            )
+            self.mssim_sum += mssim
+
+        self.num_images += batch_size
+
+        return self
 
 class PSNR():
     def __init__(self, data_range, device):
@@ -33,7 +73,7 @@ class PSNR():
     
 class SSIM():
     def __init__(self, device):
-        self.ssim = StructuralSimilarity(device=device)
+        self.ssim = StructuralSimilarity_gray(device=device)
 
     def __call__(self, outputs, targets):
         self.ssim.update(outputs, targets)
