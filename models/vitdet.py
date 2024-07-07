1f6748d1d039ed2f01b54f9e697f7357f32b9ee9
teddy <teddytw1209@gmail.com>
2024-07-07 15:33:36 +0800

    Update vitdet.py

--- models/vitdet.py
+++ models/vitdet.py
@@ -204,8 +204,8 @@ class LayerNorm(nn.Module):
 
     def __init__(self, normalized_shape: int, eps: float=1e-6, spatial_dims: int = 2,):
         super().__init__()
-        self.weight = nn.Parameter(torch.empty(normalized_shape))
-        self.bias = nn.Parameter(torch.empty(normalized_shape))
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
         self.eps = eps
         self.spatial_dims = spatial_dims
         self.normalized_shape = tuple([normalized_shape]+[1 for i in range(self.spatial_dims)])
@@ -220,13 +220,11 @@ class LayerNorm(nn.Module):
         Returns:
             normaled x: tensor with channel first (B,C,H,W) or (B,C,H,W,D)
         """
-        '''
         u = x.mean(1, keepdim=True)
         s = (x - u).pow(2).mean(1, keepdim=True)
         x = (x - u) / torch.sqrt(s + self.eps)
-        x = self.weight[:, None, None] * x + self.bias[:, None, None]
-        '''
-        return F.layer_norm(x, self.normalized_shape, self.weight.reshape(self.normalized_shape), self.bias.reshape(self.normalized_shape), self.eps)
+        x = self.weight.reshape(self.normalized_shape) * x + self.reshape(self.normalized_shape)
+        return x
 
     def reset_parameters(self) -> None:
         init.ones_(self.weight)
